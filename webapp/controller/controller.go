023c69199360a2c429646a5bd8fb8b779fd8df68
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2020-12-23 15:24:24 +0800

    added response headers

--- webapp/controller/controller.go
+++ webapp/controller/controller.go
@@ -12,19 +12,45 @@ func SetupRoutes() {
 	handleStatic("fonts")
 	handleStatic("js")
 
-	http.HandleFunc("/", app.Home)
-	http.HandleFunc("/login", app.Login)
-	http.HandleFunc("/admin/users", app.Users)
-	http.HandleFunc("/admin/newUser", app.NewUser)
-	http.HandleFunc("/admin/status", app.Status)
-	http.HandleFunc("/admin/delete", app.Delete)
-	http.HandleFunc("/register", app.Register)
-	http.HandleFunc("/operations", app.Todo)
-	http.HandleFunc("/logout", app.Logout)
+	handleFunc("/", noCache(app.Home))
+	handleFunc("/login", noCache(app.Login))
+	handleFunc("/admin/users", noCache(app.Users))
+	handleFunc("/admin/newUser", noCache(app.NewUser))
+	handleFunc("/admin/status", app.Status)
+	handleFunc("/admin/delete", app.Delete)
+	handleFunc("/register", noCache(app.Register))
+	handleFunc("/operations", app.Todo)
+	handleFunc("/logout", app.Logout)
 
-	//http.HandleFunc("/websocket", api.Endpoints)
+	//handleFunc("/websocket", api.Endpoints)
 }
 
 func handleStatic(path string) {
 	http.Handle("/"+path+"/", http.StripPrefix("/"+path+"/", http.FileServer(http.Dir("./static/"+path))))
+}
+
+func handleFunc(pattern string, h func(http.ResponseWriter, *http.Request)) {
+	http.HandleFunc(pattern, securityHeaders(h))
+}
+
+func securityHeaders(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000 ; includeSubDomains")
+		w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; script-src 'self' 'unsafe-inline'; connect-src 'self'; font-src 'self'; style-src 'self';")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Permitted-Cross-Domain-Policies", "none")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+		h(w, r)
+	}
+}
+
+func noCache(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		h(w, r)
+	}
 }
