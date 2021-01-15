cc7921c4675721b6b12e1e8ab6914f38e36197b5
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2021-01-15 21:17:52 +0800

    variable renaming

--- webapp/controller/controller.go
+++ webapp/controller/controller.go
@@ -59,9 +59,10 @@ func handleFunc(pattern string, h func(http.ResponseWriter, *http.Request)) {
 func securityHeaders(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.RequestURI)
-		token := session.GetSession(w, r).SetCSPNonce()
+		nonce := session.GetSession(w, r).SetCSPNonce()
+		log.Printf("CSP nonce: %s\n", nonce)
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000 ; includeSubDomains")
-		w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; connect-src wss://"+config.Config.Hostname+"; font-src 'self'; img-src 'self'; style-src 'self' 'nonce-"+token+"'; form-action 'self'; base-uri 'self'; script-src 'nonce-"+token+"'")
+		w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; connect-src wss://"+config.Config.Hostname+"; font-src 'self'; img-src 'self'; style-src 'self' 'nonce-"+nonce+"'; form-action 'self'; base-uri 'self'; script-src 'nonce-"+nonce+"'")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Permitted-Cross-Domain-Policies", "none")
 		w.Header().Set("Referrer-Policy", "no-referrer")
