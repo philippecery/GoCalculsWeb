60a82ea98f2fddc72c84a39e8b1ca6c028c1d6a4
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2021-01-17 18:31:31 +0800

    displaying lastconnection

--- webapp/controller/app/common/profile.go
+++ webapp/controller/app/common/profile.go
@@ -6,56 +6,67 @@ import (
 
 	"github.com/philippecery/maths/webapp/database/dataaccess"
 	"github.com/philippecery/maths/webapp/database/document"
+	"github.com/philippecery/maths/webapp/i18n"
 
 	"github.com/philippecery/maths/webapp/controller/app"
 	"github.com/philippecery/maths/webapp/session"
 )
 
+// Profile handles requests to /profile
+// Only GET and POST requests are allowed. The user must be authenticated to access this page.
+//  - a GET request will display the Profile form. If an error message is available in the session, it will be displayed.
+//  - a POST request will update the User document in database if the submitted data are valid.
 func Profile(w http.ResponseWriter, r *http.Request) {
 	if httpsession := session.GetSession(w, r); httpsession != nil {
 		if user := httpsession.GetAuthenticatedUser(); user != nil {
-			if token := httpsession.GetCSRFToken(); token != "" {
-				if r.Method == "GET" {
-					vd := app.NewViewData(w, r)
-					vd.SetUser(user)
-					vd.SetViewData("UserProfile", dataaccess.GetUserByID(user.UserID))
-					vd.SetToken(token)
-					vd.SetErrorMessage(httpsession.GetErrorMessageID())
-					vd.SetDefaultLocalizedMessages().
-						AddLocalizedMessage("userid").
-						AddLocalizedMessage("lastConnection").
-						AddLocalizedMessage("firstName").
-						AddLocalizedMessage("lastName").
-						AddLocalizedMessage("emailAddress").
-						AddLocalizedMessage("save").
-						AddLocalizedMessage("cancel")
-					if err := app.Templates.ExecuteTemplate(w, "profile.html.tpl", vd); err != nil {
-						log.Fatalf("Error while executing template 'profile': %v\n", err)
-					}
-					return
-				}
-				if r.Method == "POST" {
-					if r.PostFormValue("token") == token {
-						if r.PostFormValue("userId") == user.UserID {
-							if userProfile, err := validateUserInput(r); err == nil {
-								if err := dataaccess.UpdateUserProfile(userProfile); err != nil {
-									httpsession.SetErrorMessageID("errorGenericMessage")
-								}
-							} else {
-								httpsession.SetErrorMessageID(err.Error())
-							}
-							http.Redirect(w, r, "/profile", http.StatusFound)
-							return
+			if userProfile := dataaccess.GetUserByID(user.UserID); userProfile != nil {
+				if token := httpsession.GetCSRFToken(); token != "" {
+					if r.Method == "GET" {
+						vd := app.NewViewData(w, r)
+						vd.SetUser(user)
+						vd.SetViewData("UserProfile", userProfile)
+						vd.SetViewData("LastConnection", i18n.FormatDateTime(userProfile.LastConnection, vd.GetCurrentLanguage()))
+						vd.SetToken(token)
+						vd.SetErrorMessage(httpsession.GetErrorMessageID())
+						vd.SetDefaultLocalizedMessages().
+							AddLocalizedMessage("profile").
+							AddLocalizedMessage("userid").
+							AddLocalizedMessage("lastConnection").
+							AddLocalizedMessage("firstName").
+							AddLocalizedMessage("lastName").
+							AddLocalizedMessage("emailAddress").
+							AddLocalizedMessage("save").
+							AddLocalizedMessage("cancel")
+						if err := app.Templates.ExecuteTemplate(w, "profile.html.tpl", vd); err != nil {
+							log.Fatalf("Error while executing template 'profile': %v\n", err)
 						}
-						log.Println("/profile: Invalid User ID")
+						return
+					}
+					if r.Method == "POST" {
+						if r.PostFormValue("token") == token {
+							if r.PostFormValue("userId") == user.UserID {
+								if userProfile, err := validateUserInput(r); err == nil {
+									if err := dataaccess.UpdateUserProfile(userProfile); err != nil {
+										httpsession.SetErrorMessageID("errorGenericMessage")
+									}
+								} else {
+									httpsession.SetErrorMessageID(err.Error())
+								}
+								http.Redirect(w, r, "/profile", http.StatusFound)
+								return
+							}
+							log.Println("/profile: Invalid User ID")
+						} else {
+							log.Println("/profile: Invalid CSRF token")
+						}
 					} else {
-						log.Println("/profile: Invalid CSRF token")
+						log.Printf("/profile: Invalid method %s\n", r.Method)
 					}
 				} else {
-					log.Printf("/profile: Invalid method %s\n", r.Method)
+					log.Println("/profile: CSRF token not found in session")
 				}
 			} else {
-				log.Println("/profile: CSRF token not found in session")
+				log.Println("/profile: User not found in database")
 			}
 		} else {
 			log.Println("/profile: User is not authenticated")
