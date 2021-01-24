20c143845f15fd8d03d35f70aad28b1118dfd2eb
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2021-01-24 12:00:37 +0800

    adding self to CSP connect-src for ajax calls

--- webapp/controller/controller.go
+++ webapp/controller/controller.go
@@ -67,7 +67,7 @@ func securityHeaders(h func(http.ResponseWriter, *http.Request, *session.HTTPSes
 		if httpsession := session.GetSession(w, r); httpsession != nil {
 			nonce := httpsession.SetCSPNonce()
 			w.Header().Set("Strict-Transport-Security", "max-age=31536000 ; includeSubDomains")
-			w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; connect-src wss://"+config.Config.Hostname+"; font-src 'self'; img-src 'self'; style-src 'self' 'nonce-"+nonce+"'; form-action 'self'; base-uri 'self'; script-src 'nonce-"+nonce+"'")
+			w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; connect-src wss://"+config.Config.Hostname+" 'self'; font-src 'self'; img-src 'self'; style-src 'self' 'nonce-"+nonce+"'; form-action 'self'; base-uri 'self'; script-src 'nonce-"+nonce+"'")
 			w.Header().Set("X-Content-Type-Options", "nosniff")
 			w.Header().Set("X-Permitted-Cross-Domain-Policies", "none")
 			w.Header().Set("Referrer-Policy", "no-referrer")
