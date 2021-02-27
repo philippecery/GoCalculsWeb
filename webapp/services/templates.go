cf67628f90ba9d160279eb097ea6630725359d73
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2021-02-27 17:48:21 +0800

    template file extension

--- webapp/services/templates.go
+++ webapp/services/templates.go
@@ -6,7 +6,7 @@ import "text/template"
 var Templates = template.New("")
 
 func init() {
-	Templates = template.Must(Templates.ParseGlob("templates/*.tpl"))
-	Templates = template.Must(Templates.ParseGlob("templates/*/*.tpl"))
-	Templates = template.Must(Templates.ParseGlob("templates/*/*/*.tpl"))
+	Templates = template.Must(Templates.ParseGlob("templates/*.tmpl"))
+	Templates = template.Must(Templates.ParseGlob("templates/*/*.tmpl"))
+	Templates = template.Must(Templates.ParseGlob("templates/*/*/*.tmpl"))
 }
