626a1386a98b5502db0682c55284cf4cd0e1d3b2
Philippe David Céry <73773471+philippecery@users.noreply.github.com>
2021-05-13 20:24:30 +0800

    template path fix

--- webapp/services/templates.go
+++ webapp/services/templates.go
@@ -1,12 +1,16 @@
 package services
 
-import "text/template"
+import (
+	"log"
+	"text/template"
+)
 
 // Templates contains all the existing HTML templates
 var Templates = template.New("")
 
 func init() {
-	Templates = template.Must(Templates.ParseGlob("templates/*.tmpl"))
-	Templates = template.Must(Templates.ParseGlob("templates/*/*.tmpl"))
-	Templates = template.Must(Templates.ParseGlob("templates/*/*/*.tmpl"))
+	Templates = template.Must(Templates.ParseGlob("/templates/*.tmpl"))
+	Templates = template.Must(Templates.ParseGlob("/templates/*/*.tmpl"))
+	Templates = template.Must(Templates.ParseGlob("/templates/*/*/*.tmpl"))
+	log.Printf("%d templates found", len(Templates.Templates()))
 }
